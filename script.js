d075f01da0a72b026324b1336a63fe34332a83ec
ShhPanda <pdmdavid@gmail.com>
2023-08-28 16:28:19 +0800

    encode url

--- script.js
+++ script.js
@@ -11,12 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function applyTransformations(url) {
-    // Add your transformation logic here
-    // For example, you can add query parameters to the URL
-    //const transformedUrl = url + "?width=300&height=200";
     const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/";
     const modifiedImageUrl = new URL(url);
-    const modifiedPath = modifiedImageUrl.pathname.replace(/\//g, "_");
-    const transformedUrl = modifiedImageUrl.protocol + cloudfrontUrl + modifiedImageUrl.hostname + modifiedPath;
+    const modifiedPath = encodeURI(modifiedImageUrl.hostname + modifiedImageUrl.pathname);
+    //const modifiedPath = modifiedImageUrl.pathname.replace(/\//g, "_");
+    const transformedUrl = modifiedImageUrl.protocol + cloudfrontUrl + modifiedPath;
     return transformedUrl;
 }
