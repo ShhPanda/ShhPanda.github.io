9be3896a9349042d10d7a1f296dcff7c17393ca3
ShhPanda <pdmdavid@gmail.com>
2023-08-28 16:40:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     //const imageUrl = "https://arweave.net/fQdZjoDxvChU1R4NJ4miTcLbLJQq5Lbf3WJLCU6evKI";
+    const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/";
     const imageUrl = "https://metadata.degods.com/g/3676-dead.png"; // Replace with the actual image URL
-    const transformedImageUrl = applyTransformations(imageUrl);
+    const modifiedImageUrl = new URL(imageUrl);
+    const finalString = modifiedImageUrl.protocol + cloudfrontUrl 
+        + encodeURIComponent(modifiedImageUrl.hostname + modifiedImageUrl.pathname);
+    console.log(finalString);
 
     const transformedLinkElement = document.getElementById("transformedLink");
-    transformedLinkElement.textContent = transformedImageUrl;
+    transformedLinkElement.textContent = finalString;
 
     const imgElement = document.getElementById("transformedImage");
-    imgElement.src = transformedImageUrl;
+    imgElement.src = finalString;
 });
-
-function applyTransformations(url) {
-    const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/";
-    const modifiedImageUrl = new URL(url);
-    const modifiedPath = encodeURI(modifiedImageUrl.hostname + modifiedImageUrl.pathname);
-    //const modifiedPath = modifiedImageUrl.pathname.replace(/\//g, "_");
-    const transformedUrl = modifiedImageUrl.protocol + cloudfrontUrl + modifiedPath;
-    console.log(transformedUrl);
-    return transformedUrl;
-}
