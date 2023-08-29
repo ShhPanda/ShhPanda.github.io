f8eef46656d9f33530ffc049a7ae533140138854
ShhPanda <pdmdavid@gmail.com>
2023-08-29 11:35:34 +0800

    change to image with query

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     //const imageUrl = "https://arweave.net/fQdZjoDxvChU1R4NJ4miTcLbLJQq5Lbf3WJLCU6evKI";
     const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/";
-    const imageUrl = "https://metadata.degods.com/g/2510.png"; // Replace with the actual image URL
+    const imageUrl = "https://metadata.degods.com/g/2510.png?width=540&height=675"; // Replace with the actual image URL
     const modifiedImageUrl = new URL(imageUrl);
     const finalString = modifiedImageUrl.protocol + cloudfrontUrl 
         + modifiedImageUrl.hostname + modifiedImageUrl.pathname;
