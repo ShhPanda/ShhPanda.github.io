8766b2e4a7ffc8964810e41486ba733463d40ab5
ShhPanda <pdmdavid@gmail.com>
2023-08-30 17:04:13 +0800

    include images to uri

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     //const imageUrl = "https://arweave.net/fQdZjoDxvChU1R4NJ4miTcLbLJQq5Lbf3WJLCU6evKI";
-    const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/";
+    const cloudfrontUrl = "//d23nwyc0vr15d5.cloudfront.net/images/";
     const imageUrl = "https://metadata.degods.com/g/4947.png"; // Replace with the actual image URL
     const modifiedImageUrl = new URL(imageUrl);
     const finalString = modifiedImageUrl.protocol + cloudfrontUrl 
